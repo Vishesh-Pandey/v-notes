eff2b8004b9bf3daf119ce4c005d1c2f93e8a1de
VisheshPandey <visheshpandey24@gmail.com>
2022-08-10 00:22:39 +0530

    added alert to handle bug

--- src/components/Login.js
+++ src/components/Login.js
@@ -75,7 +75,7 @@ function Login() {
             alert("Please try again")
         }
         else if (flag === 0) {
-            alert("Incorrect username or password")
+            alert("Incorrect username or password. If correct click on login again.")
         }
 
     }
@@ -112,6 +112,19 @@ function Login() {
                             </div>
                             <div className="col-12 my-3">
                                 <button className="btn btn-secondary bg-opacity-25 w-75" onClick={signUp}>SignUp</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="alert alert-danger d-flex align-items-center" role="alert">
+                            <svg width={20} xmlns="http://www.w3.org/2000/svg" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                            </svg>
+                            <div>
+                                Please <b>don't use </b> auto-complete feature of your keyboard. Also username and password is <b>case-sensitive</b>
                             </div>
                         </div>
                     </div>

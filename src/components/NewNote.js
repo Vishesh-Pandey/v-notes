80082579d304703c1351d66ed3d014cbff3362fd
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2022-10-31 23:11:28 +0530

    added useLocation to highlight nav-items + signout

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,50 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function NewNote(props) {
-    const [title, setTitle] = useState("");
-    const [note, setNote] = useState("");
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
 
-    function save() {
-        props.save(props.user, title, note);
-    }
+  function save() {
+    props.save(props.user, title, note);
+  }
 
-    function handleTitle(event) {
-        setTitle(event.target.value)
-    }
+  function handleTitle(event) {
+    setTitle(event.target.value);
+  }
 
-    function handleNote(event) {
-        setNote(event.target.value)
-    }
+  function handleNote(event) {
+    setNote(event.target.value);
+  }
 
-    return (
-        <>
-            <div className="card text-bg-light mb-3">
-                <div className="card-header">Create a new Note here..</div>
-                <div className="card-body">
-                    <h5 className="card-title"><input type="text" value={title} onChange={handleTitle} placeholder='Write title...' className='w-75 border-0' />
-                    </h5>
-                    <div className="card-text my-2">
-                        <div className="form-floating">
-                            <textarea
-                                value={note}
-                                onChange={handleNote}
-                                className="form-control"
-                                placeholder="Leave a comment here"
-                                id="floatingTextarea2"
-                                style={{ height: 100 }}
-                            />
-                            <label htmlFor="floatingTextarea2">Notes...</label>
-                        </div>
-                    </div>
-                    <button onClick={save} className="btn btn-secondary">Save Note </button>
-                    <button onClick={props.cancelNote} className="btn btn-secondary mx-2">Cancel </button>
-                </div>
+  return (
+    <>
+      <div className="card text-bg-light mb-3">
+        <div className="card-header">Create a new Note here..</div>
+        <div className="card-body">
+          <h5 className="card-title">
+            <input
+              type="text"
+              value={title}
+              onChange={handleTitle}
+              placeholder="Write title..."
+              className="w-75 border-0"
+            />
+          </h5>
+          <div className="card-text my-2">
+            <div className="form-floating">
+              <textarea
+                value={note}
+                onChange={handleNote}
+                className="form-control"
+                placeholder="Leave a comment here"
+                id="floatingTextarea2"
+                style={{ height: 100 }}
+              />
+              <label htmlFor="floatingTextarea2">Notes...</label>
             </div>
-
-
-
-        </>
-    )
+          </div>
+          <button onClick={save} className="btn btn-secondary">
+            Save Note{" "}
+          </button>
+          <button onClick={props.cancelNote} className="btn btn-secondary mx-2">
+            Cancel{" "}
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default NewNote
+export default NewNote;
